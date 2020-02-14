4189f47c93c9de00c60154ccbaaff0798d1c69dc
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 07:23:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(string)
+def reverse_each_word_with_each(string)
   starting_array = string.split(" ")
   array = []
   starting_array.each do|string|
@@ -7,7 +7,7 @@ def reverse_each_word(string)
   array.join(" ")
 end
 
-reverse_each_word_with_collect(string)
+reverse_each_word(string)
   convert_array = string.split(" ")
   test_array = []
   convert_array.collect do|string|
