37c54e3941d4082f1f44b37b756e13705885a846
Daniel Urbina <danielur1999@gmail.com>
2020-02-14 07:31:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,1 @@
-def reverse_each_word(string)
-  starting_array = string.split(" ")
-  array = []
-  starting_array.each do|string|
-    array << string.reverse 
-  end
-  array.join(" ")
-end
-
-reverse_each_word_with_collect(string)
-  
-end 
 
